84dfa291b205c89c3ce01277961e0f08f7ac7bd1
Alessandro Comodi <acomodi@antmicro.com>
2021-03-22 14:22:31 +0100

    xc7: automate "big" cells XML generation
    Signed-off-by: Alessandro Comodi <acomodi@antmicro.com>

--- utils/build_primitive_proto.py
+++ utils/build_primitive_proto.py
@@ -11,8 +11,9 @@ some manual adjustments.
 import argparse
 import os
 import json
-import re
 import lxml.etree as ET
+
+from sdf_timing.sdfparse import parse
 
 BIN = "BIN"
 BOOL = "BOOL"
@@ -20,91 +21,188 @@ INT = "INT"
 STR = "STR"
 
 
-def build_pb_type_prototype(ports, attrs, name):
-    pb_type_xml = ET.Element(
-        "pb_type", {
-            "name": name.upper(),
-            "num_pb": "1",
-            "blif_model": ".subckt {}_VPR".format(name.upper())
-        }
-    )
+def is_clock_delay(pin, clock_ports):
+    return any(pin == clock_port["name"] for clock_port in clock_ports)
 
-    endswith_int = re.compile("[0-9]*$")
+
+def get_ports_dictionary(ports):
+    ports_dict = {"input": list(), "output": list(), "clock": list()}
+
     for port_name, config in ports.items():
-        int_present = re.search(endswith_int, port_name).group(0)
-        trimmed_port_name = port_name.strip(int_present)
-        input_type = config["direction"]
+        direction = config["direction"]
         width = config["width"]
-        if trimmed_port_name.endswith("CLK"):
-            input_type = "clock"
 
-        ET.SubElement(
-            pb_type_xml, input_type, {
-                "name": port_name,
-                "num_pins": str(width),
+        port_attrs = {
+            "name": port_name,
+            "num_pins": str(width),
+        }
+
+        ports_dict[direction].append(port_attrs)
+
+    return ports_dict
+
+
+def build_pb_type_prototype(
+        ports, attrs, primitive, delay_data, filter_clock_delays,
+        pre_built_pb_type
+):
+
+    params_list = list()
+    if pre_built_pb_type is not None:
+        parser = ET.XMLParser(remove_blank_text=True)
+        pb_type_xml = ET.parse(pre_built_pb_type, parser=parser).getroot()
+        fasm_params_xml = pb_type_xml.xpath("//meta[@name='fasm_params']")[0]
+        fasm_params_list = [
+            x.strip() for x in fasm_params_xml.text.split('\n') if x.strip()
+        ]
+
+        for param in fasm_params_list:
+            fields = [x.strip() for x in param.split("=")]
+            param_key = fields[0]
+            param_value = fields[1]
+
+            params_list.append((param_key, param_value))
+    else:
+        pb_type_xml = ET.Element(
+            "pb_type", {
+                "name": primitive.upper(),
+                "num_pb": "1",
+                "blif_model": ".subckt {}_VPR".format(primitive.upper())
             }
         )
 
-    xml_fasm_params = "\n"
+        metadata = ET.SubElement(pb_type_xml, "metadata")
+        fasm_params_xml = ET.SubElement(
+            metadata, "meta", {"name": "fasm_params"}
+        )
+
+    ports_dict = get_ports_dictionary(ports)
+
+    for port_type, port_list in ports_dict.items():
+        for port in sorted(port_list, key=lambda port: port["name"]):
+            ET.SubElement(pb_type_xml, port_type, port)
+
+    delays = list()
+    for type, name, from_pin, to_pin in delay_data:
+        is_clk_dly = is_clock_delay(from_pin, ports_dict["clock"])
+
+        if is_clk_dly and from_pin in filter_clock_delays:
+            continue
+
+        if type == "hold":
+            assert is_clk_dly, from_pin
+            delay_name = "T_hold"
+            delay_attrs = {
+                "value": "{%s}" % name,
+                "port": to_pin,
+                "clock": from_pin
+            }
+        elif type == "setup":
+            assert is_clk_dly
+            delay_name = "T_setup"
+            delay_attrs = {
+                "value": "{%s}" % name,
+                "port": to_pin,
+                "clock": from_pin
+            }
+        elif type == "iopath" and is_clk_dly:
+            delay_name = "T_clock_to_Q"
+            delay_attrs = {
+                "max": "{%s}" % name,
+                "port": to_pin,
+                "clock": from_pin
+            }
+        elif type == "iopath":
+            delay_name = "delay_constant"
+            delay_attrs = {
+                "max": "{%s}" % name,
+                "out_port": to_pin,
+                "in_port": from_pin
+            }
+        else:
+            assert False, delay_data
+
+        port_name = to_pin if is_clk_dly else from_pin
+
+        delays.append((delay_name, delay_attrs, port_name))
+
+    for delay_name, delay_attrs, _ in sorted(delays, key=lambda port: port[0]):
+        ET.SubElement(pb_type_xml, delay_name, delay_attrs)
+
     for attr_name, config in attrs.items():
         attr_type = config["type"]
         values = config["values"]
         digits = config["digits"]
         if attr_type == BIN or attr_type == INT:
-            fasm_param = "      {}[{}:0] = {}\n".format(
-                attr_name, digits - 1, attr_name
-            )
+            attr_key = "{}[{}:0]".format(attr_name, digits - 1)
+            attr_value = attr_name
+
+            params_list.append((attr_key, attr_value))
         elif attr_type == BOOL:
-            fasm_param = "      {} = {}\n".format(attr_name, attr_name)
+            params_list.append((attr_name, attr_name))
         else:
             assert attr_type == STR
             for val in values:
-                fasm_param = "      {}.{} = {}_{}\n".format(
-                    attr_name, val, attr_name, val
-                )
-        xml_fasm_params += fasm_param
+                attr_key = "{}.{}".format(attr_name, val)
+                attr_value = "{}_{}".format(attr_name, val)
+                params_list.append((attr_key, attr_value))
 
-    metadata = ET.SubElement(pb_type_xml, "metadata")
-    meta = ET.SubElement(metadata, "meta", {"name": "fasm_params"})
-    meta.text = xml_fasm_params
+    params_txt = "\n"
+    for param_key, param_value in params_list:
+        params_txt += "{} = {}\n".format(param_key, param_value)
 
-    with open("{}.pb_type.xml".format(name), "w") as f:
-        xml_str = ET.tostring(pb_type_xml, pretty_print=True).decode("utf-8")
-        f.write(xml_str)
+    fasm_params_xml.text = params_txt
+
+    return pb_type_xml
 
 
-def build_model_prototype(ports, name):
-    model_xml = ET.Element("models")
-    model = ET.SubElement(
-        model_xml, "model", {"name": "{}_VPR".format(name.upper())}
+def build_model_prototype(ports, name, delay_data, filter_clock_delays):
+    xi_url = "http://www.w3.org/2001/XInclude"
+    ET.register_namespace('xi', xi_url)
+    models_xml = ET.Element("models", {}, nsmap={"xi": xi_url})
+    model_xml = ET.SubElement(
+        models_xml, "model", {"name": "{}_VPR".format(name.upper())}
     )
-    input_ports = ET.SubElement(model, "input_ports")
-    output_ports = ET.SubElement(model, "output_ports")
+    input_ports = ET.SubElement(model_xml, "input_ports")
+    output_ports = ET.SubElement(model_xml, "output_ports")
 
-    endswith_int = re.compile("[0-9]*$")
+    ports_dict = get_ports_dictionary(ports)
+
     for port_name, config in ports.items():
         input_type = config["direction"]
-        int_present = re.search(endswith_int, port_name).group(0)
-        trimmed_port_name = port_name.strip(int_present)
-        if trimmed_port_name.endswith("CLK"):
-            ET.SubElement(
-                input_ports, "port", {
-                    "name": port_name,
-                    "is_clock": "1"
-                }
-            )
-        elif input_type == "input":
-            ET.SubElement(input_ports, "port", {
-                "name": port_name,
-            })
-        else:
-            ET.SubElement(output_ports, "port", {
-                "name": port_name,
-            })
 
-    with open("{}.model.xml".format(name), "w") as f:
-        xml_str = ET.tostring(model_xml, pretty_print=True).decode("utf-8")
-        f.write(xml_str)
+        attrs = {"name": port_name}
+
+        if input_type == "clock":
+            selected_ports = input_ports
+            attrs["is_clock"] = "1"
+        elif input_type == "input":
+            selected_ports = input_ports
+
+            for type, _, from_pin, to_pin in delay_data:
+                if type == "iopath" and port_name == from_pin:
+                    if "combinational_sink_ports" not in attrs:
+                        attrs["combinational_sink_ports"] = to_pin
+                    else:
+                        attrs["combinational_sink_ports"] += " {}".format(
+                            to_pin
+                        )
+                elif to_pin == port_name and from_pin not in filter_clock_delays:
+                    attrs["clock"] = from_pin
+                    break
+        else:
+            selected_ports = output_ports
+
+            for type, _, from_pin, to_pin in delay_data:
+                is_clk_dly = is_clock_delay(from_pin, ports_dict["clock"])
+
+                if to_pin == port_name and is_clk_dly and from_pin not in filter_clock_delays:
+                    attrs["clock"] = from_pin
+
+        assert selected_ports is not None
+        ET.SubElement(selected_ports, "port", attrs)
+
+    return models_xml
 
 
 def build_cells_prototypes(ports, attrs, name):
@@ -190,34 +288,103 @@ def build_cells_prototypes(ports, attrs, name):
         f.write(verilog_map_module)
 
 
+def get_delay_data(sdf_timings_dir, primitive, ports):
+    """
+    This function retrieves timing data from the SDF files in the prjxray-db.
+
+    It returns a list with all timing annotation to add in the
+    pb_type and model of the input primitve.
+    """
+
+    bels = set()
+    primitive_delays = set()
+    for filename in os.listdir(sdf_timings_dir):
+        if not filename.endswith(".sdf"):
+            continue
+
+        with open(os.path.join(sdf_timings_dir, filename), "r") as fp:
+            timings = parse(fp.read())
+
+        for cell, cell_data in timings["cells"].items():
+            for instance, instance_data in cell_data.items():
+
+                if instance != primitive:
+                    continue
+
+                bels.add(cell)
+
+                for delay_name, delay_data in instance_data.items():
+                    to_pin = delay_data["to_pin"]
+                    from_pin = delay_data["from_pin"]
+                    type = delay_data["type"]
+
+                    if to_pin not in ports:
+                        continue
+
+                    if from_pin not in ports:
+                        continue
+
+                    primitive_delays.add((type, delay_name, from_pin, to_pin))
+
+    blk_tl_primitive = "BLK-TL-{}".format(primitive)
+    bels_json = {primitive: {primitive: {blk_tl_primitive: list()}}}
+    for bel in bels:
+        bels_json[primitive][primitive][blk_tl_primitive].append(bel)
+
+    return primitive_delays, bels_json
+
+
 def main():
     parser = argparse.ArgumentParser(
-        description=
-        "Create prototypes for model, pb_type and cells_sim/map modules "\
-        "from attributes and ports files"
+        description="Creates XML and techmap files for given primitives."
     )
     parser.add_argument(
-        "--arch", help="Architectures available: artix7, kintex7, zynq7"
+        "--arch",
+        default="artix7",
+        choices=["artix7", "zynq7", "kintex7"],
+        help="Architectures available: artix7, kintex7, zynq7"
     )
     parser.add_argument(
-        "--name",
-        help=
-        "Name of the site used to import JSONs: e.g. gtpe2_channel, pcie_2_1"
+        "--prjxray-db", required=True, help="Path to prjxray-db directory"
     )
-    parser.add_argument("--prjxray-db", help="Path to prjxray-db directory")
+    parser.add_argument(
+        "--primitive", required=True, help="Name of the primitive."
+    )
+    parser.add_argument(
+        "--build_cells",
+        action="store_true",
+        help="Enable building also the cells_map and cells_sim files."
+    )
+    parser.add_argument(
+        "--output-dir", default=os.getcwd(), help="Output directory."
+    )
+    parser.add_argument(
+        "--pre-built-pb-type",
+        default=None,
+        help="Path to a pre-built pb_type with initial configurations."
+    )
+    parser.add_argument(
+        "--filter-clock-delays",
+        default="",
+        help="Comma separated list of clocks to filter out from delays"
+    )
 
     args = parser.parse_args()
 
-    assert args.arch in ["artix7", "kintex7", "zynq7"]
+    primitive = args.primitive
 
     attrs_file = os.path.join(
-        args.prjxray_db, args.arch, "{}_attrs.json".format(args.name)
+        args.prjxray_db, args.arch, "cells_data",
+        "{}_attrs.json".format(primitive)
     )
     ports_file = os.path.join(
-        args.prjxray_db, args.arch, "{}_ports.json".format(args.name)
+        args.prjxray_db, args.arch, "cells_data",
+        "{}_ports.json".format(primitive)
     )
 
-    assert os.path.exists(attrs_file) and os.path.exists(ports_file)
+    assert os.path.exists(attrs_file) and os.path.exists(ports_file), (
+        attrs_file, ports_file
+    )
 
     with open(attrs_file, "r") as f:
         attrs = json.load(f)
@@ -225,9 +392,34 @@ def main():
     with open(ports_file) as f:
         ports = json.load(f)
 
-    build_model_prototype(ports, args.name)
-    build_pb_type_prototype(ports, attrs, args.name)
-    build_cells_prototypes(ports, attrs, args.name)
+    sdf_timings_dir = os.path.join(args.prjxray_db, args.arch, "timings")
+    delay_data, bels_json = get_delay_data(
+        sdf_timings_dir, primitive.upper(), ports
+    )
+
+    filter_clock_delays = args.filter_clock_delays.split(",")
+    model_xml = build_model_prototype(
+        ports, primitive, delay_data, filter_clock_delays
+    )
+    pb_type_xml = build_pb_type_prototype(
+        ports, attrs, primitive, delay_data, filter_clock_delays,
+        args.pre_built_pb_type
+    )
+    if args.build_cells:
+        build_cells_prototypes(ports, attrs, primitive)
+
+    model_file = "{}.model.xml".format(primitive)
+    with open(os.path.join(args.output_dir, model_file), "w") as f:
+        xml_str = ET.tostring(model_xml, pretty_print=True).decode("utf-8")
+        f.write(xml_str)
+
+    pb_type_file = "{}.pb_type.xml".format(primitive)
+    with open(os.path.join(args.output_dir, pb_type_file), "w") as f:
+        xml_str = ET.tostring(pb_type_xml, pretty_print=True).decode("utf-8")
+        f.write(xml_str)
+
+    with open(os.path.join(args.output_dir, "bels.json"), "w") as f:
+        json.dump(bels_json, f, indent=4)
 
 
 if __name__ == "__main__":
--- xc/common/cmake/project_ray.cmake
+++ xc/common/cmake/project_ray.cmake
@@ -738,3 +738,76 @@ function(PROJECT_RAY_TILE_CAPACITY)
     )
   add_file_target(FILE ${TILE_LOWER}.tile.xml GENERATED)
 endfunction()
+
+function(PRJRAY_GENERATE_PRIMITIVE)
+  # ~~~
+  # PRJRAY_GENERATE_PRIMITIVE(
+  #   PRJRAY_DB_DIR <datbase directory>
+  #   PRIMITIVE <primitive name>
+  #   [PRE_BUILT_PB_TYPE <pre-built pb_type XML>]
+  #   [FILTER_CLOCK_DELAYS <list of clocks to filter>]
+  #   )
+  # ~~~
+  #
+  # This function generates targets to build pb_types and models for
+  # primitives automatically.
+  #
+  # Arguments:
+  #   - PRJRAY_DB_DIR: directory to the device database
+  #   - PRIMITIVE: lower case primitive name
+  #   - PRE_BUILT_PB_TYPE: optional XML of a pre-written pb_type which has non-standard tags.
+  #   - FILTER_CLOCK_DELAYS: comma-separated list of clocks that need to be filtered when generating
+  #                          clock-related timing delays. Some ports do have multiple clock delay
+  #                          specifications. VPR can only accept one clock corresponding to a specific port.
+
+  set(options)
+  set(oneValueArgs PRJRAY_DB_DIR PRIMITIVE PRE_BUILT_PB_TYPE FILTER_CLOCK_DELAYS)
+  set(multiValueArgs)
+  cmake_parse_arguments(
+    PRJRAY_GENERATE_PRIMITIVE
+    "${options}"
+    "${oneValueArgs}"
+    "${multiValueArgs}"
+    ${ARGN}
+  )
+
+  set(DB_DIR ${PRJRAY_GENERATE_PRIMITIVE_PRJRAY_DB_DIR})
+  set(PRIMITIVE ${PRJRAY_GENERATE_PRIMITIVE_PRIMITIVE})
+  set(PRE_BUILT_PB_TYPE ${PRJRAY_GENERATE_PRIMITIVE_PRE_BUILT_PB_TYPE})
+  set(FILTER_CLOCK_DELAYS ${PRJRAY_GENERATE_PRIMITIVE_FILTER_CLOCK_DELAYS})
+
+  set(GENERATE_PRIMITIVE ${symbiflow-arch-defs_SOURCE_DIR}/utils/build_primitive_proto.py)
+  set(PB_TYPE_XML ${CMAKE_CURRENT_BINARY_DIR}/${PRIMITIVE}.pb_type.xml)
+  set(MODEL_XML ${CMAKE_CURRENT_BINARY_DIR}/${PRIMITIVE}.model.xml)
+
+  set(DEPS "")
+  set(EXTRA_ARGS "")
+  if(DEFINED PRE_BUILT_PB_TYPE)
+    list(APPEND DEPS ${PRE_BUILT_PB_TYPE})
+    add_file_target(FILE ${PRE_BUILT_PB_TYPE})
+    list(APPEND EXTRA_ARGS "--pre-built-pb-type" ${PRE_BUILT_PB_TYPE})
+  endif()
+
+  if(DEFINED FILTER_CLOCK_DELAYS)
+    list(APPEND EXTRA_ARGS "--filter-clock-delays" ${FILTER_CLOCK_DELAYS})
+  endif()
+
+  add_custom_command(
+    OUTPUT
+      ${PB_TYPE_XML}
+      ${MODEL_XML}
+    COMMAND
+      ${CMAKE_COMMAND} -E env PYTHONPATH=${PRJRAY_DIR}:${symbiflow-arch-defs_SOURCE_DIR}/utils
+      ${PYTHON3} ${GENERATE_PRIMITIVE}
+        --primitive ${PRIMITIVE}
+        --prjxray-db ${DB_DIR}
+        ${EXTRA_ARGS}
+      DEPENDS
+        ${DEPS}
+        ${GENERATE_PRIMITIVE}
+    )
+
+  add_file_target(FILE ${PRIMITIVE}.pb_type.xml GENERATED)
+  add_file_target(FILE ${PRIMITIVE}.model.xml GENERATED)
+
+endfunction()
