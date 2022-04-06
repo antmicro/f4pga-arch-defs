ec488b0b48123801bab7530532c43717217fec0b
Paweł Czarnecki <pczarnecki@antmicro.com>
2022-04-06 11:02:09 +0200

    utils: remove vpr_fixup_post_synth.py script
    Signed-off-by: Paweł Czarnecki <pczarnecki@antmicro.com>

--- quicklogic/common/cmake/quicklogic_install.cmake
+++ quicklogic/common/cmake/quicklogic_install.cmake
@@ -128,10 +128,6 @@ function(DEFINE_QL_TOOLCHAIN_TARGET)
           DESTINATION bin/python/lib
           PERMISSIONS WORLD_READ OWNER_WRITE OWNER_READ GROUP_READ)
 
-  install(FILES ${symbiflow-arch-defs_SOURCE_DIR}/utils/vpr_fixup_post_synth.py
-          DESTINATION bin/python
-          PERMISSIONS WORLD_READ OWNER_WRITE OWNER_READ GROUP_READ)
-
   install(FILES ${symbiflow-arch-defs_SOURCE_DIR}/utils/yosys_fixup_cell_names.py
           DESTINATION bin/python
           PERMISSIONS WORLD_READ OWNER_WRITE OWNER_READ GROUP_READ)
