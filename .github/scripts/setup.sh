eee1127c6511780f5a5a47946bfe53376b8d66ef
Pawel Czarnecki <pczarnecki@antmicro.com>
2022-07-25 11:59:19 +0200

    TEMP: env
    Signed-off-by: Pawel Czarnecki <pczarnecki@antmicro.com>

--- .github/scripts/setup.sh
+++ .github/scripts/setup.sh
@@ -4,36 +4,36 @@ source $(dirname "$0")/common.sh
 set -e
 
 start_section "symbiflow.install" "Installing packages"
-apt update
-apt install -y \
-        bash \
-        bison \
-        build-essential \
-        ca-certificates \
-        clang-format \
-        cmake \
-        colordiff \
-        coreutils \
-        curl \
-        flex \
-        git \
-        graphviz \
-        inkscape \
-        jq \
-        make \
-        ninja-build \
-        nodejs \
-        openjdk-11-jdk \
-        pixz \
-        python \
-        python3 \
-        python3-dev \
-        python3-pytest \
-        python3-virtualenv \
-        python3-yaml \
-        psmisc \
-        virtualenv \
-        wget
+#apt update
+#apt install -y \
+        #bash \
+        #bison \
+        #build-essential \
+        #ca-certificates \
+        #clang-format \
+        #cmake \
+        #colordiff \
+        #coreutils \
+        #curl \
+        #flex \
+        #git \
+        #graphviz \
+        #inkscape \
+        #jq \
+        #make \
+        #ninja-build \
+        #nodejs \
+        #openjdk-11-jdk \
+        #pixz \
+        #python \
+        #python3 \
+        #python3-dev \
+        #python3-pytest \
+        #python3-virtualenv \
+        #python3-yaml \
+        #psmisc \
+        #virtualenv \
+        #wget
 end_section "symbiflow.install"
 
 start_section "symbiflow.configure_cmake" "Configuring CMake (make env)"
